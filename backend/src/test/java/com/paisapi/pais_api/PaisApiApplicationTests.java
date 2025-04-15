b6c5f737fd8ab137e1a084274c1fe57964ba7831
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-04-14 23:33:12 -0300

    build: configuração de inicialização do backend

---
+++ backend/src/test/java/com/paisapi/pais_api/PaisApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paisapi.pais_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaisApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
