ba60dde3896680b2dbfe219475936c8d249813fa
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-04-16 23:52:43 -0300

    build: commit do application

---
+++ backend/src/main/java/com/paisapi/pais_api/PaisApiApplication.java
@@ -0,0 +1,13 @@
+package com.paisapi.pais_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaisApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaisApiApplication.class, args);
+	}
+
+}
