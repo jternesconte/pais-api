7abf7983553d91e02a1d8a38c1319d6128a2953b
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-04-16 23:53:15 -0300

    feat: implementa metodos do UserDetails

--- backend/src/main/java/com/paisapi/pais_api/model/Usuario.java
+++ backend/src/main/java/com/paisapi/pais_api/model/Usuario.java
@@ -1,15 +1,22 @@
 package com.paisapi.pais_api.model;
 
+import com.paisapi.pais_api.dto.RoleEnum;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "usuario",
         uniqueConstraints = {
                 @UniqueConstraint(columnNames = {"login"})
         })
-public class Usuario {
+public class Usuario implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,6 +37,9 @@ public class Usuario {
     @Column(nullable = false)
     private boolean administrador;
 
+    @Transient
+    private RoleEnum role;
+
     public Usuario() {
 
     }
@@ -39,14 +49,16 @@ public class Usuario {
         this.senha = senha;
         this.nome = nome;
         this.administrador = administrador;
+        this.role = administrador ? RoleEnum.ADMIN : RoleEnum.USER;
     }
 
-    public Long getId() {
-        return id;
+    public RoleEnum getRole() {
+        return this.administrador ? RoleEnum.ADMIN : RoleEnum.USER;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public void setRole(RoleEnum role) {
+        this.role = role;
+        this.administrador = role == RoleEnum.ADMIN;
     }
 
     public String getLogin() {
@@ -79,5 +91,44 @@ public class Usuario {
 
     public void setAdministrador(boolean administrador) {
         this.administrador = administrador;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if(this.getRole() == RoleEnum.ADMIN) {
+            return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"),new SimpleGrantedAuthority("ROLE_USER"));
+        } else {
+            return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+        }
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
     }
 }
