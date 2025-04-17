42456eb08081326fa44b1369399057286410c618
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-04-16 23:51:00 -0300

    feat: adiciona interceptor para ser utilizado antes de cada request

---
+++ backend/src/main/java/com/paisapi/pais_api/util/SecurityFilter.java
@@ -0,0 +1,47 @@
+package com.paisapi.pais_api.util;
+
+import com.paisapi.pais_api.repository.UsuarioRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    TokenService tokenService;
+
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var token = this.recuperarToken(request);
+        if(token != null) {
+            var login = tokenService.validaToken(token);
+            UserDetails usuario = usuarioRepository.findByLogin(login);
+
+            var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String recuperarToken(HttpServletRequest req) {
+        var authHeader = req.getHeader("Authorization");
+        if(authHeader == null) {
+            return null;
+        } else {
+            return authHeader.replace("Bearer ", "");
+        }
+    }
+}
