278eedcd5488ee3f4345d2834325a178b287bcce
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-04-18 16:58:17 -0300

    feat: adiciona o DTO do Usuario

---
+++ backend/src/main/java/com/paisapi/pais_api/dto/UsuarioDTO.java
@@ -0,0 +1,41 @@
+package com.paisapi.pais_api.dto;
+
+public class UsuarioDTO {
+    private String login;
+    private String senha;
+    private String nome;
+    private boolean administrador;
+
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public boolean isAdministrador() {
+        return administrador;
+    }
+
+    public void setAdministrador(boolean administrador) {
+        this.administrador = administrador;
+    }
+}
