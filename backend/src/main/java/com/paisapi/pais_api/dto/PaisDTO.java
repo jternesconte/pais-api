3778a1bad8c3cabd31b1891de2ef5dadd970382c
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-04-18 16:58:07 -0300

    feat: adiciona DTO do Pais

---
+++ backend/src/main/java/com/paisapi/pais_api/dto/PaisDTO.java
@@ -0,0 +1,31 @@
+package com.paisapi.pais_api.dto;
+
+public class PaisDTO {
+    private String nome;
+    private String sigla;
+    private String gentilico;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSigla() {
+        return sigla;
+    }
+
+    public void setSigla(String sigla) {
+        this.sigla = sigla;
+    }
+
+    public String getGentilico() {
+        return gentilico;
+    }
+
+    public void setGentilico(String gentilico) {
+        this.gentilico = gentilico;
+    }
+}
