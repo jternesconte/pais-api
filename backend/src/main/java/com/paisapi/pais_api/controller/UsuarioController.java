1ed1e8f398b091648c3c09bf0a8a6c267dacd5b5
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-04-18 16:57:57 -0300

    feat: adiciona método para verificar se é admin e muda o autenticar

--- backend/src/main/java/com/paisapi/pais_api/controller/UsuarioController.java
+++ backend/src/main/java/com/paisapi/pais_api/controller/UsuarioController.java
@@ -1,6 +1,7 @@
 package com.paisapi.pais_api.controller;
 
 import com.paisapi.pais_api.dto.UsuarioAutenticadoDTO;
+import com.paisapi.pais_api.dto.UsuarioDTO;
 import com.paisapi.pais_api.dto.UsuarioLoginDTO;
 import com.paisapi.pais_api.model.Usuario;
 import com.paisapi.pais_api.repository.UsuarioRepository;
@@ -9,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 
@@ -28,26 +31,36 @@ public class UsuarioController {
     private PasswordEncoder passwordEncoder;
 
     public UsuarioAutenticadoDTO autenticarUsuario(UsuarioLoginDTO usuarioDetalhes) {
-        var usernamePassword = new UsernamePasswordAuthenticationToken(usuarioDetalhes.getLogin(),usuarioDetalhes.getSenha());
+        try {
+            var usernamePassword = new UsernamePasswordAuthenticationToken(usuarioDetalhes.getLogin(),usuarioDetalhes.getSenha());
 
-        var auth = this.authenticationManager.authenticate(usernamePassword);
-        var token = tokenService.gerarToken((Usuario) auth.getPrincipal());
-        Usuario usuario = (Usuario) auth.getPrincipal();
+            var auth = this.authenticationManager.authenticate(usernamePassword);
+            var token = tokenService.gerarToken((Usuario) auth.getPrincipal());
+            Usuario usuario = (Usuario) auth.getPrincipal();
 
-        return new UsuarioAutenticadoDTO(
-                usuario.getLogin(),
-                usuario.getNome(),
-                token,
-                usuario.isAdministrador(),
-                true
-        );
+            return new UsuarioAutenticadoDTO(
+                    usuario.getLogin(),
+                    usuario.getNome(),
+                    token,
+                    usuario.isAdministrador(),
+                    true
+            );
+        } catch (Exception e) {
+            return new UsuarioAutenticadoDTO(
+                    usuarioDetalhes.getLogin(),
+                    null,
+                    null,
+                    false,
+                    false
+            );
+        }
     }
 
     public String gerarToken(String login) {
         return "gerado_jwt";
     }
 
-    public Usuario criarUsuario(Usuario novoUsuario) {
+    public UsuarioLoginDTO criarUsuario(UsuarioDTO novoUsuario) {
         if(usuarioRepository.findByLogin(novoUsuario.getLogin()) != null) {
             return null;
         }
@@ -61,8 +74,22 @@ public class UsuarioController {
         );
 
         usuarioRepository.save(usuario);
-        ResponseEntity.ok().build();
-        return usuario;
+        UsuarioLoginDTO usuarioLoginDTO = new UsuarioLoginDTO(
+                usuario.getLogin(),
+                usuario.getSenha()
+        );
 
+        return usuarioLoginDTO;
+
+    }
+
+    public boolean isAdministrador() {
+        var authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.getPrincipal() instanceof UserDetails) {
+            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+            return userDetails.getAuthorities().stream()
+                    .anyMatch(authority -> authority.getAuthority().equals("ROLE_ADMIN"));
+        }
+        return false;
     }
 }
