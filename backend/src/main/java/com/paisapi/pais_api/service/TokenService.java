61d41407856b58483da2b776740665f8afbaffb9
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-04-18 17:00:00 -0300

    refactor: muda forma de adicionar novo token ou atualizar o existente

--- backend/src/main/java/com/paisapi/pais_api/service/TokenService.java
+++ backend/src/main/java/com/paisapi/pais_api/service/TokenService.java
@@ -4,13 +4,18 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.paisapi.pais_api.model.Token;
 import com.paisapi.pais_api.model.Usuario;
+import com.paisapi.pais_api.repository.TokenRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.sql.Timestamp;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
+import java.util.Optional;
 
 @Service
 public class TokenService {
@@ -18,14 +23,38 @@ public class TokenService {
     @Value("${api.security.token.secret}")
     private String secret;
 
+    @Autowired
+    private TokenRepository tokenRepository;
+
     public String gerarToken(Usuario usuario) {
         try {
             Algorithm algorithm = Algorithm.HMAC256(secret);
+            Instant dataExpiracao = criaDataExpiracao();
             String token = JWT.create()
                     .withIssuer("pais-api")
                     .withSubject(usuario.getLogin())
-                    .withExpiresAt(criaDataExpiracao())
+                    .withExpiresAt(dataExpiracao)
                     .sign(algorithm);
+
+            Optional<Token> tokenExistente = tokenRepository.findByLogin(usuario.getLogin());
+
+            if(tokenExistente.isEmpty()) {
+                Token novoToken = new Token(
+                        token,
+                        usuario.getLogin(),
+                        Timestamp.from(dataExpiracao),
+                        usuario.isAdministrador()
+                );
+
+                tokenRepository.save(novoToken);
+            } else {
+                tokenExistente.get().setToken(token);
+                tokenExistente.get().setExpiracao(Timestamp.from(dataExpiracao));
+
+                tokenRepository.save(tokenExistente.get());
+            }
+
+
 
             return token;
         } catch(JWTCreationException e) {
