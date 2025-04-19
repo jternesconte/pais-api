5e644127a53fbbc0644d06cbf3635d9eddefe9e5
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-04-19 02:52:13 -0300

    feat: adicionar as rotas do app

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { PaisesComponent } from './pages/paises/paises.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   {
+      path: '',
+      component: LoginComponent
+   },
+   {
+      path: 'login',
+      component: LoginComponent
+   },
+   {
+      path: 'paises',
+      component: PaisesComponent
+   }
+];
