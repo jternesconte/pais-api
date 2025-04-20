3b370cdfff0f98ecfae2002b1d0c20c24f4ecb13
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-04-20 16:33:45 -0300

    feat: implementa uma tela inicial para a aplicação

---
+++ frontend/src/app/pages/menu-inicial/menu-inicial.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+
+@Component({
+  selector: 'app-menu-inicial',
+  templateUrl: './menu-inicial.component.html',
+  styleUrls: ['./menu-inicial.component.css'],
+  imports: [ButtonModule, CardModule, RouterModule],
+})
+export class MenuInicialComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  abrirSwagger() {
+    window.open('http://localhost:8080/swagger-ui.html', '_blank');
+  }
+
+  abrirBanco() {
+    window.open('http://localhost:8080/h2-console', '_blank');
+  }
+
+}
